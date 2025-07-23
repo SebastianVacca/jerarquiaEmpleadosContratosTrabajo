548c9f5229d8c23da0beb2962dc559c2c07a7b92
Juan Vacca <clanbs11.12@gmail.com>
2025-07-23 18:40:32 -0500

    Add class Manager

---
+++ src/Manager.java
@@ -0,0 +1,24 @@
+public class Manager extends Employee{
+    private Double bonoAnual;
+
+    public Manager(String nombre, String id, double salary, Double bonoAnual){
+        super(nombre, id, bonoAnual);
+        this.bonoAnual = bonoAnual;
+    }
+
+    public Double getBonoAnual() {
+        return bonoAnual;
+    }
+
+    @Override
+    public Double calcularSalario() {
+        return super.getSalary() + bonoAnual;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        
+        super.mostrarDetalles();
+        System.out.printf("Bono anual: %,.1f%n", getBonoAnual());
+    }
+}
